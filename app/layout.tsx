edf4f071c572b62382893777b0af26d82d8c6707
BB-Simon <simon.chowdery@gmail.com>
2023-09-12 20:26:23 +0300

    Initial project setup and add providers component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Providers } from '@components'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -16,7 +17,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+      {/* Allow for more hieght on mobile divices */}
+      {/* <div className='h-40 md:hidden' /> */}
     </html>
   )
 }
